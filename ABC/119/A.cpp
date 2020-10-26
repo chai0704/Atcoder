107ab3b5bb3001db23e739bf0257b53106ead888
chai0704 <73388956+chai0704@users.noreply.github.com>
2020-10-27 02:57:38 +0900

    Create A.cpp

---
+++ ABC/119/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll  long long 
+#define INF 999999999
+#define rep(i,n) for(int i=0;i<n;i++)
+const ll MOD = 1000000007;
+ll cnt =0,ans=0;
+//cout << setprecision(11);
+
+int main(){
+  int y,m,d;
+  char c,cc;
+  cin >> y >> c >> m >> cc >> d;
+  if(m>4)cout << "TBD" << endl;
+  else cout << "Heisei" << endl;
+}
