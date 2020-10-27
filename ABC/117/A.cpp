6187d1efff001908757f9940d8455d27a61ea1b8
chai0704 <73388956+chai0704@users.noreply.github.com>
2020-10-27 16:19:10 +0900

    Create A.cpp

---
+++ ABC/117/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll  long long 
+#define INF 999999999
+#define rep(i,n) for(int i=0;i<n;i++)
+const ll MOD = 1000000007;
+const double pi = acos(-1);
+ll cnt =0,ans=0;
+//cout << setprecision(11) << fixed;
+int main(){
+    cout << setprecision(11) << fixed;
+   double t,x; cin >> t >> x;
+   cout << t/x << endl;
+}
