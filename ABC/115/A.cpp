2319da084955f2e781c83fe524054bbabe19f4b7
chai0704 <73388956+chai0704@users.noreply.github.com>
2020-10-27 16:38:27 +0900

    Create A.cpp

---
+++ ABC/115/A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll  long long 
+#define INF 999999999
+#define rep(i,n) for(int i=0;i<n;i++)
+const ll MOD = 1000000007;
+const double pi = acos(-1);
+ll cnt =0,ans=0;
+//cout << setprecision(11) << fixed;
+int main(){
+   int d;   cin >> d;
+   if(d==25){
+       cout << "Christmas" << endl;
+      return 0; 
+   }
+   if(d==24){
+ cout << "Christmas Eve" << endl;
+      return 0; 
+   }
+   if(d==23){
+ cout << "Christmas Eve Eve " << endl;
+      return 0; 
+   }
+   if(d==22){
+ cout << "Christmas Eve Eve Eve" << endl;
+      return 0; 
+   }
+}
