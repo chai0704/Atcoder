d57d3a8dba50391dcc5b52423bc811bc570500b2
chai0704 <73388956+chai0704@users.noreply.github.com>
2020-11-27 17:51:51 +0900

    Create A.cpp

---
+++ ABC/182/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll  long long 
+const ll INF = 1LL << 60;
+#define rep(i,n) for(int i=0;i<n;i++)
+const double pi = acos(-1);
+const ll MOD = 1000000007; 
+ll cnt =0,ans=0;
+typedef pair<int , int > P;
+//cout << setprecision(11) << fixed;
+
+
+int main(){
+  int a,b,c;
+  cin >> a >> b;
+  c = 2*a+100;
+  cout << c - b << endl;
+}
