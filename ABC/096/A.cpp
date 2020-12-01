437bf9b1fad5093646230260acc42598e57abd39
chai0704 <73388956+chai0704@users.noreply.github.com>
2020-12-01 18:50:42 +0900

    Create A.cpp

---
+++ ABC/096/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int,int>;
+//using mint = modint1000000007;
+
+int main(){
+ int a,b;cin >> a >> b;
+
+ if(a < b)cout << a << endl;
+ if(a == b)cout << a << endl;
+ if(a > b)cout << a-1 << endl; 
+}
