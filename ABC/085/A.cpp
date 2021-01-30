509c2b4d3fb6c7807fc96515a21c3e4fef907e37
chai0704 <73388956+chai0704@users.noreply.github.com>
2021-01-30 20:09:31 +0900

    Create A.cpp

---
+++ ABC/085/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll =  long long;
+using P = pair<int,int>;
+//using mint = modint1000000007;
+const int MAX = 510000;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+
+int main(){
+string s;
+cin >> s;
+
+s.at(3) = '8';
+cout << s << endl;
+}
