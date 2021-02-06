5112f7bf78c344d820bec61bafd0ccb0965530f5
chai0704 <73388956+chai0704@users.noreply.github.com>
2021-02-06 11:36:30 +0900

    Create A.cpp

---
+++ ABC/083/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define ll  long long 
+const ll INF = 1LL << 60;
+#define rep(i,n) for(int i=0;i<n;i++)
+const double pi = acos(-1);
+const ll MOD = 1000000007; 
+ll cnt =0,ans=0;
+typedef pair<int , int > P;
+//cout << setprecision(11) << fixed;
+
+
+int main(){
+    int a,b,c,d;
+    cin >> a >> b >> c >>d;
+    if(a+b > c+d)cout << "Left"<< endl;
+    if(a+b == c+d)cout << "Balanced"<< endl;
+    if(a+b < d+c)cout << "Right"<< endl;
+}
